530f9ceab6d5340952ef185d13b423b06a0a8c6c
Alan_Thea <dahlan.fauzi1991@gmail.com>
2022-03-18 15:52:20 +0700

    Add files via upload

---
+++ inc.connection.php
@@ -0,0 +1,23 @@
+<?php
+# Konek ke Web Server Lokal
+$myHost  = "localhost";
+$myUser  = "root";
+$myPass  = "";
+$myDbs  = "apotek"; // nama database, disesuaikan dengan database di MySQL
+
+# Konek ke Web Server Lokal
+$koneksidb  = new mysqli($myHost, $myUser, $myPass, $myDbs);
+if (!$koneksidb) {
+  echo "Failed Connection !";
+}
+
+# Memilih database pd MySQL Server
+// mysql_select_db($myDbs) or die("Database not Found !");
+
+# Menampilkan data toko
+// $toko = mysqli_query($koneksidb, "SELECT * FROM toko WHERE id=1");
+// $ambil = mysqli_fetch_array($toko);
+$toko = $koneksidb->query("SELECT * FROM toko WHERE id=1");
+$ambil = $toko->fetch_assoc();
+
+$faktur = $ambil['kode_faktur'];
